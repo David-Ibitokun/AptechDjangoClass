797d7f8737e5073b48c144eb24c2d9ec35505d38
David-Ibitokun <123756353+David-Ibitokun@users.noreply.github.com>
2025-11-12 18:13:29 +0100

    development.yaml added

--- sumia/settings.py
+++ sumia/settings.py
@@ -187,8 +187,8 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get('STATIC_ROOT', str(BASE_DIR / 'staticfiles_collected'))
 # if not DEBUG:
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
